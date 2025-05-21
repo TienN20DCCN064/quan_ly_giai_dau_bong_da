19417390f91651cafd4c53d4d900d899bfdb43cb
crush24h <hieuahieua@gmail.com>
2025-05-22 04:25:32 +0700

    add

---
+++ backend/services/global/hamChiTiet.js
@@ -0,0 +1,116 @@
+
+const hamChiTiet = {
+    //==============Quan ly============================
+    async layDoiBongTheoQL(idQuanLy) {
+        return await layDoiBongTheoQL(idQuanLy);
+    },
+    async laycauThuTheoQuanLy(idQuanLy) {
+        return await laycauThuTheoQuanLy(idQuanLy);
+    },
+    async laycauThuTheoDoiBong(doiBongID) {
+        return await laycauThuTheoDoiBong(doiBongID);
+    },
+    //===================================Dang nhap================
+    async dangNhap(data) {
+        return await dangNhap(data);
+    }
+};
+
+//==========================Quản lý
+// lấy danh sách cầu thủ theo đội bóng
+async function laycauThuTheoDoiBong(doiBongID) {
+    //doiBongID =  localStorage.getItem("ma_doi_bong");
+    if (!doiBongID) {
+        alert('Thiếu ID đội bóng');
+        return;
+    }
+
+    try {
+        const response = await fetch(GlobalStore.getLinkCongAPI() + 'cau_thu/doibong?ma_doi_bong=' + doiBongID);
+        return await response.json();
+    } catch (error) {
+        console.error('Lỗi khi lấy danh sách đội bóng theo quản lý:', error);
+        return [];
+    }
+}
+
+// lấy danh sách cầu thủ theo quản lý
+async function laycauThuTheoQuanLy(maQL) {
+
+    if (!maQL) {
+        alert('Thiếu ID quản lý');
+        return;
+    }
+    
+    const url = GlobalStore.getLinkCongAPI() + 'cauthu/quanly?ma_ql_doi_bong=' + maQL;
+    //console.log(url);
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error('Lỗi khi lấy danh sách đội bóng theo quản lý:', error);
+        return [];
+    }
+}
+
+
+
+// lấy danh sách đội bóng theo ID quản lý
+async function layDoiBongTheoQL(maQL) {
+
+    //API mới: filter theo user name:
+    if (!maQL) {
+        alert('Thiếu ID quản lý');
+        return;
+    }
+    
+
+    const url = GlobalStore.getLinkCongAPI() + 'doi_bong/quanly?ma_ql_doi_bong=' + maQL;
+    //console.log(url);
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error('Lỗi khi lấy danh sách đội bóng theo quản lý:', error);
+        return [];
+    }
+}
+
+
+//===============================================Dang nhap
+
+async function dangNhap(data) {
+
+    const url = GlobalStore.getLinkCongAPI() + 'dangnhap';
+    try {
+        const response = await fetch(url,{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error('Lỗi khi lấy danh sách đội bóng theo quản lý:', error);
+        return [];
+    } 
+}
+
+async function getThongtinUser(data) {
+
+    const url = GlobalStore.getLinkCongAPI() + 'get_user?user_name=' + data;
+    try {
+        const response = await fetch(url,{
+            method: "Get",
+            headers: { "Content-Type": "application/json" }            
+        });
+        return await response.json();
+    } catch (error) {
+        console.error('Lỗi khi lấy thông tin user theo username:', error);
+        return [];
+    }
+}
+
+
+
+// Gắn vào window để có thể truy cập ở mọi nơi
+window.hamChiTiet = hamChiTiet;
